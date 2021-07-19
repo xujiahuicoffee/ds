a6efea6d4a15581c044e556fd82c1a901783e7f6
xujiahui <953323966@qq.com>
2021-07-19 22:55:41 +0800

    创建工程，第一次提交

---
+++ dsdatas/src/main/java/com/xjh/dsdata/DsdataApplication.java
@@ -0,0 +1,13 @@
+package com.xjh.dsdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsdataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DsdataApplication.class, args);
+    }
+
+}
---
+++ dsdatas/src/test/java/com/xjh/dsdata/DsdataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xjh.dsdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsdataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
